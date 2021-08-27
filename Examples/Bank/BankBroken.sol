d3da77ad5358d94928fb1fdbd8198cee4ff87fb6
Michael George <mdgeorge@cs.cornell.edu>
2021-08-27 09:59:18 -0400

    removed tab characters

--- Examples/Bank/BankBroken.sol
+++ Examples/Bank/BankBroken.sol
@@ -2,41 +2,40 @@ pragma solidity >= 0.4.24 < 0.8;
 
 contract Bank {
     mapping (address => uint256) public funds;
-	uint256 public totalFunds;
+    uint256 public totalFunds;
 
-	// get the current fund of an account
-	function getFunds(address account) public view returns (uint256) {
-		return funds[account];
-	}
+    // get the current fund of an account
+    function getFunds(address account) public view returns (uint256) {
+        return funds[account];
+    }
 
-	// get the total funds in the bank
-	function getTotalFunds() public view returns (uint256) {
-		return totalFunds;
-	}
+    // get the total funds in the bank
+    function getTotalFunds() public view returns (uint256) {
+        return totalFunds;
+    }
 
-	// deposit an amount to an account
+    // deposit an amount to an account
     function deposit(uint256 amount) public payable {
-		funds[msg.sender] += amount;
-		totalFunds += amount;
+        funds[msg.sender] += amount;
+        totalFunds += amount;
     }
 
-	// transfer an amount to an account
+    // transfer an amount to an account
     function transfer(address to, uint256 amount) public {
-		require(funds[msg.sender] > amount);
-		funds[msg.sender] -= amount;
-		funds[to] += amount;
-		
+        require(funds[msg.sender] > amount);
+        funds[msg.sender] -= amount;
+        funds[to] += amount;
     }
 
-	// withdraw all amounts
+    // withdraw all amounts
     function withdraw() public returns (bool success)  {
-		uint256 amount = getFunds(msg.sender);
-		funds[msg.sender] = 0;
-		success = msg.sender.send(amount);
-		totalFunds -=amount;
+        uint256 amount = getFunds(msg.sender);
+        funds[msg.sender] = 0;
+        success = msg.sender.send(amount);
+        totalFunds -=amount;
     }
-	
-	function getEthBalance(address account) public view returns (uint256){
-		return account.balance;
-	}
+
+    function getEthBalance(address account) public view returns (uint256){
+        return account.balance;
+    }
 }
